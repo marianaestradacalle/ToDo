f87e7e4502600d25ed61787fc1d855bd89d0327e
marianaestradacalle <estradacallemariana@gmail.com>
2020-06-05 17:56:59 -0500

    Script creado

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,8 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/ToDo';
+    // urlDB = 'mongodb://localhost:27017/ToDo';
+    urlDB = 'mongodb+srv://Mariec:tkG5IacnM9DoHQpM@cluster0-tfvfd.mongodb.net/ToDo?retryWrites=true&w=majority';
 } else {
     urlDB = process.env.MONGO_URL;
 }
